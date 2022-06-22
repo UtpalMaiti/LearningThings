e3c7c2d21810761cb00919f18b525e672cf554b6
Utpal Maiti <utpalmaiti12@gmail.com>
2022-06-23 03:48:20 +0530

    t

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 LearningThings
+https://github.com/orgs/LinkedInLearning/repositories
+
+ANGULAR/
+Angular API Communication and Authentication_LinkedIn'/
+Angular_Progressive_Web_Apps_LinkedIn/
+Angular2 for .NET Developers_LinkedIn'/
+angular-essential-training_LinkedIn/
+ASP.NET Core Identity Authentication Management_LinkedIn'/
+aspnetcore-azure-tables-2841537_LinkedIn/
+PWA/
+ReactSPA6_ALL/
---
+++ aspnetcore-azure-tables-2841537_LinkedIn/EmployeeManagement/Data/Operations/Operations.cs
@@ -0,0 +1,122 @@
+﻿using EmployeeManagement.Data.Entities;
+using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Data.Operations
+{
+    public class Operations
+    {
+        public async Task TriggerOperations()
+        {
+            string tableName = "Employee";
+
+            CloudTable cloudTable = await Common.Common.CreateTableAsync(tableName);
+
+            try
+            {
+                //await AddDataOperation(cloudTable);
+                //await UpdateDataOperation(cloudTable);
+                //RetrieveAllDataOperation(cloudTable);
+                EmployeeEntity entity = RetrieveSingleDataOperation(cloudTable, "Consulting", "ervis-trupja-rk-new");
+                DeleteDataOperation(cloudTable, entity);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static async Task AddDataOperation(CloudTable cloudTable)
+        {
+            //Create Entity
+            EmployeeEntity newEmployeeEntity = new EmployeeEntity("Consulting")
+            {
+                FullName = "Ervis Trupja",
+                Email = "ervis@trupja.com",
+                RowKey = "ervis-trupja-rk"
+            };
+
+            TableOperation addOperation = TableOperation.InsertOrMerge(newEmployeeEntity);
+            TableResult addResult = await cloudTable.ExecuteAsync(addOperation);
+            EmployeeEntity addResponse = addResult.Result as EmployeeEntity;
+
+            if (addResult.RequestCharge.HasValue)
+            {
+                Console.WriteLine($"addResponse ==> {addResponse.PartitionKey} - {addResponse.RowKey} - {addResponse.FullName}");
+            }
+        }
+
+        public static async Task UpdateDataOperation(CloudTable cloudTable)
+        {
+            //Create Entity
+            EmployeeEntity newEmployeeEntity = new EmployeeEntity("Consulting")
+            {
+                FullName = "Ervis Trupja New",
+                Email = "ervis@trupja.com",
+                RowKey = "ervis-trupja-rk-new"
+            };
+
+            TableOperation addOperation = TableOperation.InsertOrMerge(newEmployeeEntity);
+            TableResult addResult = await cloudTable.ExecuteAsync(addOperation);
+            EmployeeEntity addResponse = addResult.Result as EmployeeEntity;
+
+            if (addResult.RequestCharge.HasValue)
+            {
+                Console.WriteLine($"addResponse ==> {addResponse.PartitionKey} - {addResponse.RowKey} - {addResponse.FullName}");
+            }
+
+            newEmployeeEntity.FullName = "Updated Full Name";
+
+            TableOperation updateOperation = TableOperation.InsertOrMerge(newEmployeeEntity);
+            TableResult updateResult = await cloudTable.ExecuteAsync(updateOperation);
+            EmployeeEntity updateResponse = updateResult.Result as EmployeeEntity;
+
+            if (updateResult.RequestCharge.HasValue)
+            {
+                Console.WriteLine($"updateResponse ==> {updateResponse.PartitionKey} - {updateResponse.RowKey} - {updateResponse.FullName}");
+            }
+        }
+
+        public void RetrieveAllDataOperation(CloudTable cloudTable)
+        {
+            var allDataQuery = cloudTable.ExecuteQuery(new TableQuery<EmployeeEntity>());
+            foreach (var employee in allDataQuery)
+            {
+                Console.WriteLine($"RowKey: {employee.RowKey}, PartionKey: {employee.PartitionKey}, Full Name: {employee.FullName}");
+            }
+        }
+
+        public EmployeeEntity RetrieveSingleDataOperation(CloudTable cloudTable, string partitionKey, string rowKey)
+        {
+            TableOperation retrieveSingleOperation = TableOperation.Retrieve<EmployeeEntity>(partitionKey, rowKey);
+
+            TableResult result = cloudTable.Execute(retrieveSingleOperation);
+
+            EmployeeEntity employee = result.Result as EmployeeEntity;
+
+            if(employee != null)
+            {
+                Console.WriteLine($"Employee Data => {employee.RowKey},  {employee.PartitionKey},  {employee.FullName} ");
+
+                return employee;
+            }
+            return null;
+        }
+
+        public void DeleteDataOperation(CloudTable cloudTable, EmployeeEntity entity)
+        {
+            TableOperation deleteOperation = TableOperation.Delete(entity);
+
+            TableResult result = cloudTable.Execute(deleteOperation);
+
+            if (result.RequestCharge.HasValue)
+            {
+                Console.WriteLine($"Request Charge of Delete Operation : {result.RequestCharge}");
+            }
+        }
+    }
+}
