e658d0cebb34b298cca064144a3d49c6049fa466
UtpalMaiti <Utpalmaiti12@gmail.com>
2022-07-10 06:28:05 +0530

    Ex_Files_Illustrator_2022_EssT

--- PWA/MyCoffeeApp/src/app/app.module.ts
+++ PWA/MyCoffeeApp/src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeolocationService } from './geolocation.service';
 import { DataService } from './data.service';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import 'hammerjs';
+import { ListComponent } from './list/list.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CoffeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatToolbarModule
   ],
   providers: [GeolocationService,DataService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
